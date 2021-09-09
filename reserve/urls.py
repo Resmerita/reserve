055d1590147d88ff1d06a70838edcb097de373dc
Ionut Resmerita <ionut.resmerita@devnest.ro>
2021-09-09 13:54:32 +0300

    Nothing important

--- reserve/urls.py
+++ reserve/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("contact/", include("contact.urls")),
+    path("", include("contact.urls")),
 ]
