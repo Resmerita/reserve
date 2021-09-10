bd33a7a166862a22dc54f1d7ffa15cd2eb5a409d
Ionut Resmerita <ionut.resmerita@devnest.ro>
2021-09-10 16:33:55 +0300

    chenged dinamic port

--- reserve/settings.py
+++ reserve/settings.py
@@ -141,7 +141,7 @@ EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
-EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_PORT = os.environ.get("EMAIL_PORT", process.env.PORT)
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 
